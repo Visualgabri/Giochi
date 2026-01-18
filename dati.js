67861779ef25afb33a567cbef902c38cf0c9e4b3
Gabriele Belmonte <visualgabri@gmail.com>
2026-01-18 11:28:23 +0100

    Aggiornamento automatico 18/01/2026 11:28:23,37

--- dati.js
+++ dati.js
@@ -3,7 +3,7 @@ const GIOCHI_DATA = [
     "name": "index2.html",
     "path": "index2.html",
     "category": "Vari",
-    "updatedAt": 1768731999703.0845
+    "updatedAt": 1768732102124.122
   },
   {
     "name": "chris-dino-spiderman.html",
