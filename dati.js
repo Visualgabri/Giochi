e1c4c2cf7cba87ab3c2ca78303d3571cc1acb1b5
Gabriele Belmonte <visualgabri@gmail.com>
2026-01-27 21:20:04 +0100

    Aggiornamento automatico 27/01/2026 21:20:04,38

--- dati.js
+++ dati.js
@@ -1,5 +1,17 @@
 const GIOCHI_DATA = [
   {
+    "name": "colibri-julie.html",
+    "path": "Julie/colibri-julie.html",
+    "category": "Julie",
+    "updatedAt": 1769545171911.7603
+  },
+  {
+    "name": "colibri-julie-coach.html",
+    "path": "Julie/colibri-julie-coach.html",
+    "category": "Julie",
+    "updatedAt": 1769544978496.1904
+  },
+  {
     "name": "julie-amici-numeri.html",
     "path": "Julie/julie-amici-numeri.html",
     "category": "Julie",
