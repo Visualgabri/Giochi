5c77535f6ecdf1515f0354d0a8001deef28d6042
Visualgabri <visualgabri@gmail.com>
2026-04-01 09:58:39 +0200

    Aggiornamento automatico 01/04/2026 09:58

--- dati.js
+++ dati.js
@@ -1,45 +1,57 @@
 const GIOCHI_DATA = [
   {
+    "name": "pepper-prova.html",
+    "path": "Julie/Italiano/pepper-prova.html",
+    "category": "Italiano",
+    "updatedAt": 1774962795222
+  },
+  {
     "name": "julie-ahmed.html",
     "path": "Julie/Italiano/julie-ahmed.html",
     "category": "Italiano",
-    "updatedAt": 1774898377049.629
+    "updatedAt": 1774898377049
   },
   {
     "name": "julie-nonno.html",
     "path": "Julie/Italiano/julie-nonno.html",
     "category": "Italiano",
-    "updatedAt": 1774556915470.3628
+    "updatedAt": 1774556915470
   },
   {
     "name": "julie-ticche-tocche.html",
     "path": "Julie/Italiano/julie-ticche-tocche.html",
     "category": "Italiano",
-    "updatedAt": 1774296913252.9902
+    "updatedAt": 1774296913252
   },
   {
     "name": "julie-piombino.html",
     "path": "Julie/Italiano/julie-piombino.html",
     "category": "Italiano",
-    "updatedAt": 1774296540332.0457
+    "updatedAt": 1774296540332
   },
   {
     "name": "julie-filastrocca-primavera.html",
     "path": "Julie/Italiano/julie-filastrocca-primavera.html",
     "category": "Italiano",
-    "updatedAt": 1774295998504.0325
+    "updatedAt": 1774295998504
   },
   {
     "name": "julie-obiettivo9.html",
     "path": "Julie/Matematica/julie-obiettivo9.html",
     "category": "Matematica",
-    "updatedAt": 1773692932151.2424
+    "updatedAt": 1773693022707
   },
   {
     "name": "index2.html",
     "path": "index2.html",
     "category": "Vari",
-    "updatedAt": 1773568042479.6313
+    "updatedAt": 1773568105663
+  },
+  {
+    "name": "indexOld.html",
+    "path": "indexOld.html",
+    "category": "Vari",
+    "updatedAt": 1773568099647
   },
   {
     "name": "julie-amici-numeri-avanzato.html",
@@ -51,223 +63,223 @@ const GIOCHI_DATA = [
     "name": "julie-ascensore-magico.html",
     "path": "Julie/Italiano/julie-ascensore-magico.html",
     "category": "Italiano",
-    "updatedAt": 1773261376029.2744
+    "updatedAt": 1773261376029
   },
   {
     "name": "Chris-Simulatore-Macchina.html",
     "path": "Chris/Chris-Simulatore-Macchina.html",
     "category": "Chris",
-    "updatedAt": 1772989887063.49
+    "updatedAt": 1772989887063
   },
   {
     "name": "Julie-Sottrazioni6.html",
     "path": "Julie/Matematica/Julie-Sottrazioni6.html",
     "category": "Matematica",
-    "updatedAt": 1772989686404.867
+    "updatedAt": 1772989686404
   },
   {
     "name": "julie-letture-maiu-minus.html",
     "path": "Julie/Italiano/julie-letture-maiu-minus.html",
     "category": "Italiano",
-    "updatedAt": 1772989511339.0752
+    "updatedAt": 1772989511339
   },
   {
     "name": "julie-sottrazioni5.html",
     "path": "Julie/Matematica/julie-sottrazioni5.html",
     "category": "Matematica",
-    "updatedAt": 1772989389590.644
+    "updatedAt": 1772989389590
   },
   {
     "name": "julie-sottrazioni4.html",
     "path": "Julie/Matematica/julie-sottrazioni4.html",
     "category": "Matematica",
-    "updatedAt": 1772742107881.1296
+    "updatedAt": 1772742107881
   },
   {
     "name": "julie-sottrazioni3.html",
     "path": "Julie/Matematica/julie-sottrazioni3.html",
     "category": "Matematica",
-    "updatedAt": 1772741183039.403
+    "updatedAt": 1772741183039
   },
   {
     "name": "julie-sottrazioni2.html",
     "path": "Julie/Matematica/julie-sottrazioni2.html",
     "category": "Matematica",
-    "updatedAt": 1772740990384.6506
+    "updatedAt": 1772740990384
   },
   {
     "name": "julie-sottrazioni1.html",
     "path": "Julie/Matematica/julie-sottrazioni1.html",
     "category": "Matematica",
-    "updatedAt": 1772740803110.5247
+    "updatedAt": 1772740803110
   },
   {
     "name": "julie-lettura-minuscolo.html",
     "path": "Julie/Italiano/julie-lettura-minuscolo.html",
     "category": "Italiano",
-    "updatedAt": 1772570724778.0662
+    "updatedAt": 1772570724778
   },
   {
     "name": "julie-frecce.html",
     "path": "Julie/Matematica/julie-frecce.html",
     "category": "Matematica",
-    "updatedAt": 1772570489099.5957
+    "updatedAt": 1772570489099
   },
   {
     "name": "julie-lettere-minuscole.html",
     "path": "Julie/Italiano/julie-lettere-minuscole.html",
     "category": "Italiano",
-    "updatedAt": 1772444240480.3098
+    "updatedAt": 1772444240480
   },
   {
     "name": "juie-ombra-sofia.html",
     "path": "Julie/Italiano/juie-ombra-sofia.html",
     "category": "Italiano",
-    "updatedAt": 1771964115408.5667
+    "updatedAt": 1771964115408
   },
   {
     "name": "julie-in-classe.html",
     "path": "Julie/Italiano/julie-in-classe.html",
     "category": "Italiano",
-    "updatedAt": 1771695083791.682
+    "updatedAt": 1771695083791
   },
   {
     "name": "julie-gelato.html",
     "path": "Julie/Italiano/julie-gelato.html",
     "category": "Italiano",
-    "updatedAt": 1770367860776.337
+    "updatedAt": 1770367860776
   },
   {
     "name": "prova.html",
     "path": "Palestra 2026/prova.html",
     "category": "Palestra 2026",
-    "updatedAt": 1770275667052.943
+    "updatedAt": 1770275667052
   },
   {
     "name": "sifda-numeri-succ-prec.html",
     "path": "Julie/Matematica/sifda-numeri-succ-prec.html",
     "category": "Matematica",
-    "updatedAt": 1770019346882.4277
+    "updatedAt": 1770019346882
   },
   {
     "name": "julie-gufo.html",
     "path": "Julie/Italiano/julie-gufo.html",
     "category": "Italiano",
-    "updatedAt": 1769767083476.7278
+    "updatedAt": 1769767083476
   },
   {
     "name": "julie-colibri-coach-tablet.html",
     "path": "Julie/Italiano/julie-colibri-coach-tablet.html",
     "category": "Italiano",
-    "updatedAt": 1769545672668.3594
+    "updatedAt": 1769545672668
   },
   {
     "name": "colibri-julie-coach.html",
     "path": "Julie/Italiano/colibri-julie-coach.html",
     "category": "Italiano",
-    "updatedAt": 1769545249526.2986
+    "updatedAt": 1769545249526
   },
   {
     "name": "colibri-julie.html",
     "path": "Julie/Italiano/colibri-julie.html",
     "category": "Italiano",
-    "updatedAt": 1769545232510.165
+    "updatedAt": 1769545232510
   },
   {
     "name": "julie-amici-numeri.html",
     "path": "Julie/Matematica/julie-amici-numeri.html",
     "category": "Matematica",
-    "updatedAt": 1769375700044.6023
+    "updatedAt": 1769375700044
   },
   {
     "name": "julie-contadinella-icone.html",
     "path": "Julie/Italiano/julie-contadinella-icone.html",
     "category": "Italiano",
-    "updatedAt": 1769375662717.1604
+    "updatedAt": 1769375662717
   },
   {
     "name": "julie-lucia-contadinella.html",
     "path": "Julie/Italiano/julie-lucia-contadinella.html",
     "category": "Italiano",
-    "updatedAt": 1769160596178.1892
+    "updatedAt": 1769160596178
   },
   {
     "name": "chris-dino-spiderman.html",
     "path": "Chris/chris-dino-spiderman.html",
     "category": "Chris",
-    "updatedAt": 1768759161816.519
-  },
-  {
-    "name": "indexOld.html",
-    "path": "indexOld.html",
-    "category": "Vari",
-    "updatedAt": 1768758979817.324
+    "updatedAt": 1768759161816
   },
   {
     "name": "prenotazioneGoogle.html",
     "path": "prenotazioneGoogle.html",
     "category": "Vari",
-    "updatedAt": 1768758594050.533
+    "updatedAt": 1768758594050
+  },
+  {
+    "name": "Babbo-Natale-Giroscopio.html",
+    "path": "Chris/Babbo-Natale-Giroscopio.html",
+    "category": "Chris",
+    "updatedAt": 1768731791192
   },
   {
     "name": "julie-spesa-emma.html",
     "path": "Julie/Italiano/julie-spesa-emma.html",
     "category": "Italiano",
-    "updatedAt": 1768730431509.3186
+    "updatedAt": 1768730431509
   },
   {
     "name": "julie-amici-numeri-evoluto.html",
     "path": "Julie/Matematica/julie-amici-numeri-evoluto.html",
     "category": "Matematica",
-    "updatedAt": 1768726760394.3345
+    "updatedAt": 1768726760394
   },
   {
     "name": "julie-spesa-emma (con icone).html",
     "path": "Julie/Italiano/julie-spesa-emma (con icone).html",
     "category": "Italiano",
-    "updatedAt": 1768576713664.9185
+    "updatedAt": 1768576713664
   },
   {
     "name": "julie-amici-del-10.html",
     "path": "Julie/Matematica/julie-amici-del-10.html",
     "category": "Matematica",
-    "updatedAt": 1768249179222.6995
+    "updatedAt": 1768249179222
   },
   {
     "name": "julie-fatina-denti.html",
     "path": "Julie/Italiano/julie-fatina-denti.html",
     "category": "Italiano",
-    "updatedAt": 1768248885269.0195
+    "updatedAt": 1768248885269
   },
   {
     "name": "pianta-doni-julie.html",
     "path": "Julie/Italiano/pianta-doni-julie.html",
     "category": "Italiano",
-    "updatedAt": 1767900885234.379
+    "updatedAt": 1767900885234
   },
   {
     "name": "shangai.html",
     "path": "Chris/shangai.html",
     "category": "Chris",
-    "updatedAt": 1767898689159.8967
+    "updatedAt": 1767898689159
   },
   {
     "name": "Julie-lettura-nonno-tito.html",
     "path": "Julie/Italiano/Julie-lettura-nonno-tito.html",
     "category": "Italiano",
-    "updatedAt": 1766677347280.504
+    "updatedAt": 1766677347280
   },
   {
     "name": "julie-noi-al-mattino.html",
     "path": "Julie/Italiano/julie-noi-al-mattino.html",
     "category": "Italiano",
-    "updatedAt": 1766677224411.9775
+    "updatedAt": 1766677224411
   },
   {
     "name": "scarabeo-julie.html",
     "path": "Julie/Italiano/scarabeo-julie.html",
     "category": "Italiano",
-    "updatedAt": 1766321880347.9019
+    "updatedAt": 1766321880347
   },
   {
     "name": "corsa-chris.html",
@@ -279,7 +291,7 @@ const GIOCHI_DATA = [
     "name": "corsa-chris-computer.html",
     "path": "Chris/Bolle Famiglia/corsa-chris-computer.html",
     "category": "Bolle Famiglia",
-    "updatedAt": 1765968318088.4814
+    "updatedAt": 1765968318088
   },
   {
     "name": "Chris-babbo-natale-voci-estese.html",
@@ -291,25 +303,25 @@ const GIOCHI_DATA = [
     "name": "famiglia-memory.html",
     "path": "Chris/Bolle Famiglia/famiglia-memory.html",
     "category": "Bolle Famiglia",
-    "updatedAt": 1765727060536.3713
+    "updatedAt": 1765727060536
   },
   {
     "name": "bolle-famiglia.html",
     "path": "Chris/Bolle Famiglia/bolle-famiglia.html",
     "category": "Bolle Famiglia",
-    "updatedAt": 1765726892788.6091
+    "updatedAt": 1765726892788
   },
   {
     "name": "julie-lettura-notte-2.html",
     "path": "Julie/Italiano/julie-lettura-notte-2.html",
     "category": "Italiano",
-    "updatedAt": 1765707361458.0706
+    "updatedAt": 1765707361458
   },
   {
     "name": "julie-lettura-notte.html",
     "path": "Julie/Italiano/julie-lettura-notte.html",
     "category": "Italiano",
-    "updatedAt": 1765706578501.6235
+    "updatedAt": 1765706578501
   },
   {
     "name": "chris-palloncini.html",
@@ -321,30 +333,24 @@ const GIOCHI_DATA = [
     "name": "super-julie-math.html",
     "path": "Julie/Matematica/super-julie-math.html",
     "category": "Matematica",
-    "updatedAt": 1765570951784.444
+    "updatedAt": 1765570951784
   },
   {
     "name": "Julie-amici-cifre.html",
     "path": "Julie/Matematica/Julie-amici-cifre.html",
     "category": "Matematica",
-    "updatedAt": 1765533107686.7617
-  },
-  {
-    "name": "Babbo-Natale-Giroscopio.html",
-    "path": "Chris/Babbo-Natale-Giroscopio.html",
-    "category": "Chris",
-    "updatedAt": 1765407795940.8347
+    "updatedAt": 1765533107686
   },
   {
     "name": "Julie-parole-sfidanti.html",
     "path": "Julie/Italiano/Julie-parole-sfidanti.html",
     "category": "Italiano",
-    "updatedAt": 1765188738236.573
+    "updatedAt": 1765188738236
   },
   {
     "name": "Julie-parole-semplici.html",
     "path": "Julie/Italiano/Julie-parole-semplici.html",
     "category": "Italiano",
-    "updatedAt": 1765179457908.349
+    "updatedAt": 1765179457908
   }
 ];
